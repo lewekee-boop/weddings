fc8dd2ac4954114ebd07d873a57e8dff4ed42b8c
lewekee-boop <lewekee@gmail.com>
2026-06-26 12:57:17 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const invitation = {
-  date: "15 августа 2026",
-  time: "16:00",
+  date: "5 сентября 2026",
+  time: "15:00",
   place: "Дворец бракосочетания, Тюмень, ул. Малыгина, 85",
   mapUrl: "https://maps.google.com/?q=Дворец+бракосочетания+Тюмень+улица+Малыгина+85",
 };
 
-const dateEl = document.querySelector("#event-date");
-const timeEl = document.querySelector("#event-time");
-const placeEl = document.querySelector("#event-place");
 const mapLink = document.querySelector("#map-link");
 
-dateEl.textContent = invitation.date;
-timeEl.textContent = invitation.time;
-placeEl.textContent = invitation.place;
 mapLink.href = invitation.mapUrl;
